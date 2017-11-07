52e5ccda1013316f4485b5b050ea722f46f6c47c
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2017-11-07 16:08:31 +0100

    0.0.3

---
+++ README.md
@@ -0,0 +1,10 @@
+# Draconius Text
+
+```javascript
+
+const strings = require('dracotext').load('english');
+
+strings.get('key.system.application.name');
+strings.getCreature('MONSTER_WATER_14');
+
+```
--- build/test.js
+++ build/test.js
@@ -1,4 +1,4 @@
-const strings = require('../index')();
+const strings = require('../index').load();
 
 console.log('App Name = ' + strings.get('key.system.application.name'));
 console.log('Creature MONSTER_WATER_14 = ' + strings.getCreature('MONSTER_WATER_14'));
